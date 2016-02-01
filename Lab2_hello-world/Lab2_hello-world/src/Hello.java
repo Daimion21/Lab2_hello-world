d7722e174cc2af224df9715d50f7a2a1bc133219
Daimion21 <andrew.pytlak@maine.edu>
2016-02-01 13:48:25 -0500

    Initial Master branch commit

--- Lab2_hello-world/Lab2_hello-world/src/Hello.java
+++ Lab2_hello-world/Lab2_hello-world/src/Hello.java
@@ -5,17 +5,9 @@ public class Hello {
 	 * Implement the application
 	 * @param args arguments
 	 */
-<<<<<<< HEAD
+	
 	public static void main(String[] args) {
-		
-		System.out.println("Here ye Here ye : important announcement");
-		
-=======
-	public static void main(String[] args) {		
->>>>>>> refs/heads/master
 		System.out.println("Lab2 Hello World");
-		
-		System.out.println("Lab2 Goodbye World");
 	}
 
 }
