aaa91d2141876c79d6c7b998bf4c9b11bcbb16a5
Daimion21 <andrew.pytlak@maine.edu>
2016-02-01 14:10:21 -0500

    Slave 1branch commit

--- Lab2_hello-world/Lab2_hello-world/src/Hello.java
+++ Lab2_hello-world/Lab2_hello-world/src/Hello.java
@@ -8,6 +8,8 @@ public class Hello {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Here ye Here ye : important announcement");
+		
 		System.out.println("Lab2 Hellow World");
 	}
 
